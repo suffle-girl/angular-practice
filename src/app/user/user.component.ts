915766f7a0f116cbb8894da90355506fd1ea04f2
Šárka F. <sarka.flidrova@email.cz>
2025-05-08 15:43:55 +0200

    TS - type and interface.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) user!: {
-    id: string;
-    name: string;
-    avatar: string;
-  };
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
