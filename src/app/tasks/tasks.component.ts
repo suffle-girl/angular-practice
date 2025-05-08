67e9395d91a1912ab9f3148a33277b8585ecd7b0
Šárka F. <sarka.flidrova@email.cz>
2025-05-08 13:28:49 +0200

    Create a configurable tasks component.

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) name!: string;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,8 +9,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // @Output() select = new EventEmitter();ˇ
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return '/users/' + this.avatar;
