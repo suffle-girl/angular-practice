4a1f991812cb4e51eb24f1be4f1e9efd05b7a8b3
Šárka F. <sarka.flidrova@email.cz>
2025-05-12 11:29:06 +0200

    Add task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
+  imports: [TaskComponent],
 })
 export class TasksComponent {
   @Input() name?: string;
