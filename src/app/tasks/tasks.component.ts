5af0e50efedd0d5927eb76c6795f5780ce4c251a
Šárka F. <sarka.flidrova@email.cz>
2025-05-22 11:54:06 +0200

    Migrating all components to module based components.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent, NewTaskComponent],
+  standalone: false,
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
